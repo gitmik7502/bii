9a3b1212dc343b58ee59c55d391ec6237453f57b
gitmik7502 <108491891+gitmik7502@users.noreply.github.com>
2023-04-12 20:39:14 +0100

    Delete OS/test_files directory

--- OS/test_files/test2.py
+++
@@ -1,5 +0,0 @@
-import test_
-print(test_.prog_tiles())
-i = input()
-if i in test_.prog_tiles():
-    print(i)
--- OS/test_files/test_/__init__.py
+++
@@ -1,6 +0,0 @@
-def prog_tiles():
-    import importlib
-    from test_.prog1 import title as prog1title
-    prog2 = importlib.import_module("test_.prog 2")
-    prog_titles = [prog1title(), prog2.title()]
-    return prog_titles
